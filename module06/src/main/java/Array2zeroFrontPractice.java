1c0d24f829b1dba4139e57640444fe6e4e837912
Zephyr Fang <zephyr1483@gmail.com>
2026-02-17 21:56:24 -0800

    Update Array2zeroFrontPractice.java

--- module06/src/main/java/Array2zeroFrontPractice.java
+++ module06/src/main/java/Array2zeroFrontPractice.java
@@ -8,17 +8,16 @@
  * Return an array that contains the exact same numbers as the given array,
  * but rearranged so that all the zeros are grouped at the start of the array.
  * The order of the non-zero numbers does not matter.
- * You may modify and return the given array or make a new array.
  */
 
-public class YunmengzeFang {
+public class Array2zeroFrontPractice {
 
     public int[] zeroFront(int[] nums) {
 
         int[] result = new int[nums.length];
         int index = 0;
 
-        // First pass: place all zeros at the front
+        // First pass: place zeros at the front
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 0) {
                 result[index] = 0;
@@ -26,7 +25,7 @@ public class YunmengzeFang {
             }
         }
 
-        // Second pass: place non-zero numbers
+        // Second pass: place non-zero values
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
                 result[index] = nums[i];

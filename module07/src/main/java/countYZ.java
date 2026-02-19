8a6b159188e0fead8b1a5ad7cbba820399907bd0
Jessy-Ding <89423283+Jessy-Ding@users.noreply.github.com>
2026-02-18 17:30:16 -0800

    create class

---
+++ module07/src/main/java/countYZ.java
@@ -0,0 +1,20 @@
+/**
+ * CodingBat: String-3 > countYZ
+ * Given a string, count the number of words ending in 'y' or 'z'
+ * -- so the 'y' in "heavy" and the 'z' in "fez" count, but not the 'y' in "yellow" (not case sensitive).
+ * We'll say that a y or z is at the end of a word if there is not an alphabetic letter immediately following it.
+ * (Note: Character.isLetter(char) tests if a char is an alphabetic letter.)
+ * <p>
+ * {@code
+ * countYZ("fez day") → 2
+ * countYZ("day fez") → 2
+ * countYZ("day fyyyz") → 2
+ * }
+ * </p>
+ *
+ * @author Mengyuan Ding
+ * @since 2026-02-18
+ * course CS5004 (Object-Oriented Programming)
+ */
+public class countYZ {
+}

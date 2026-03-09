39f324bbb8897dcf6b8d5291237856b756cb9189
Jessy-Ding <89423283+Jessy-Ding@users.noreply.github.com>
2026-03-09 14:50:55 -0700

    REFACTOR: replace Module08Example with ScoresIncreasing class and implementation

---
+++ module08/src/main/java/ScoresIncreasing.java
@@ -0,0 +1,27 @@
+/**
+ * CodingBat: AP-1 > scoresIncreasing
+ * Given an array of scores, return true if each score is equal or greater than the one before. The array will be length 2 or more.
+ * <p>
+ * {@code
+ * scoresIncreasing([1, 3, 4]) → true
+ * scoresIncreasing([1, 3, 2]) → false
+ * scoresIncreasing([1, 1, 4]) → true
+ * }
+ * </p>
+ *
+ * @author Mengyuan Ding
+ * @since 2026-02-18
+ * course CS5004 (Object-Oriented Programming)
+ */
+public class ScoresIncreasing {
+    /**
+     * Returns the sum of two integers.
+     *
+     * @param a first number
+     * @param b second number
+     * @return a + b
+     */
+    public int add(int a, int b) {
+        return a + b;
+    }
+}

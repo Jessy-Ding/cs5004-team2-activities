9f92da6a312361054800e7772a15a7a29ba5cbf6
Zephyr Fang <zephyr1483@gmail.com>
2026-03-11 12:05:12 -0700

    Update Array2either24Practice.java

--- module07/src/main/java/Array2either24Practice.java
+++ module07/src/main/java/Array2either24Practice.java
@@ -3,8 +3,6 @@
  * Course: CS5004
  * Date: February 18, 2026
  *
- * Assignment: CodingBat Array-2 - either24
- *
  * Given an array of ints, return true if the array contains
  * a 2 next to a 2 or a 4 next to a 4, but not both.
  *

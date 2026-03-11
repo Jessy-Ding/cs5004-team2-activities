5933d6fd8d1311095c48556dd8942ba23ad9fd3d
Zephyr Fang <zephyr1483@gmail.com>
2026-03-11 12:04:51 -0700

    Update Array2either24PracticeTest.java

--- module07/src/test/java/Array2either24PracticeTest.java
+++ module07/src/test/java/Array2either24PracticeTest.java
@@ -1,34 +1,33 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 /**
- * @author: Yunmengze Fang
+ * Name: Yunmengze Fang
  * Course: CS5004
- * @since: February 18, 2026
+ * Date: February 18, 2026
  *
  * Test class for either24 method.
+ *
+ * @author Yunmengze Fang
+ * @since February 18, 2026
  */
-
-import java.util.Arrays;
-
 public class Array2either24PracticeTest {
 
-    public static void main(String[] args) {
-
+    @Test
+    public void testEither24Case1() {
         Array2either24Practice y = new Array2either24Practice();
+        assertTrue(y.either24(new int[]{1, 2, 2}));
+    }
 
-        int[] test1 = {1, 2, 2};
-        System.out.println("Test 1 Input:  " + Arrays.toString(test1));
-        System.out.println("Expected:      true");
-        System.out.println("Actual:        " + y.either24(test1));
-        System.out.println();
+    @Test
+    public void testEither24Case2() {
+        Array2either24Practice y = new Array2either24Practice();
+        assertTrue(y.either24(new int[]{4, 4, 1}));
+    }
 
-        int[] test2 = {4, 4, 1};
-        System.out.println("Test 2 Input:  " + Arrays.toString(test2));
-        System.out.println("Expected:      true");
-        System.out.println("Actual:        " + y.either24(test2));
-        System.out.println();
-
-        int[] test3 = {4, 4, 1, 2, 2};
-        System.out.println("Test 3 Input:  " + Arrays.toString(test3));
-        System.out.println("Expected:      false");
-        System.out.println("Actual:        " + y.either24(test3));
+    @Test
+    public void testEither24Case3() {
+        Array2either24Practice y = new Array2either24Practice();
+        assertFalse(y.either24(new int[]{4, 4, 1, 2, 2}));
     }
 }

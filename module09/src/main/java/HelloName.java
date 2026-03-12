2a1d4657918b94a3d6d60a13c61f7176e9201a3a
yihongkun <yihongkun513@gmail.com>
2026-03-11 17:39:55 -0700

    Add module09 HelloName practice problem

---
+++ module09/src/main/java/HelloName.java
@@ -0,0 +1,19 @@
+/**
+ * CodingBat: String-1 > helloName.
+ * Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+ *
+ * @author Hongkun Yi
+ * @since 2026-03-11
+ * course CS5004
+ */
+public class HelloName {
+    /**
+     * Returns a greeting string in the form "Hello [name]!".
+     *
+     * @param name the name to greet
+     * @return greeting string
+     */
+    public String helloName(String name) {
+        return "Hello " + name + "!";
+    }
+}
